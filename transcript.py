e39dfd6151cb7cfd4f642e0cc77159001812eb66
Ismail Badawi <isbadawi@gmail.com>
2012-03-24 00:28:16 -0400

    revamp scraping to include semester & gpa information

--- transcript.py
+++ transcript.py
@@ -19,14 +19,37 @@ def _build_course(raw_course):
 
 @cached
 def _get_transcript():
+    _semester = re.compile('(Fall|Winter|Summer)')
+    def _semester_or_course(tag):
+        return ((tag.name == 'td' and tag.has_key('nowrap')) or
+                (tag.name == 'b' and _semester.match(tag.text)))
+
     html = BeautifulSoup(browser.open(urls['transcript']))
-    courses = html.find_all('td', attrs={'nowrap': ''})
-    courses = [
-        [t.get_text() for t in courses[i:i+11] if t.get_text()] 
-        for i in range(0, len(courses), 11)
-    ]
-    courses = [_build_course(raw_course) for raw_course in courses]
-    return courses
+    all_courses = html.find_all(_semester_or_course)
+    semesters = [t.parent for t in html.find_all(text=_semester)][2:]
+    indices = [all_courses.index(t) for t in semesters]
+    term_gpas = [t.parent.parent.next_sibling.next_sibling.span.text 
+                for t in html.find_all(text="TERM GPA:")]
+    cum_gpas = [t.parent.parent.next_sibling.next_sibling.span.text 
+                for t in html.find_all(text="CUM GPA:")]
+    transcript = []
+    for i, (index, semester) in enumerate(zip(indices, semesters)):
+        semester = semester.text.replace(u'\xa0', u' ')
+        next = indices[i+1] if i < len(indices) - 1 else len(all_courses)
+        courses = [
+            [t.get_text() for t in all_courses[j:j+11] if t.get_text()]
+            for j in range(index+1, next, 11)
+        ]
+        courses = [_build_course(c) for c in courses]
+        gpa = term_gpas[i] if i < len(term_gpas) else None
+        cum_gpa = cum_gpas[i] if i < len(cum_gpas) else None
+        transcript.append({
+            'semester': semester,
+            'courses': courses,
+            'gpa': gpa,
+            'cum_gpa': cum_gpa,
+        })
+    return transcript
 
 def get(subject=None, title=None, grade=None, average=None, credits=None):
     """
