bbb543ba23c0901d9e4d7a1e38311bf38662521e
Ismail Badawi <isbadawi@gmail.com>
2013-08-25 00:27:13 -0400

    Add setup.py, publish on PyPI

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='mcgill-minerva',
+    version='0.1.0',
+    description='Client library for McGill Minerva.',
+    url='https://github.com/isbadawi/minerva',
+    author='Ismail Badawi',
+    author_email='isbadawi@gmail.com',
+    packages=['minerva'],
+    scripts=['bin/fetch-mcgill-transcript'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
