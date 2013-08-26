d2454a34e7cafeae6587654c8683c09e152e9e39
Ismail Badawi <isbadawi@gmail.com>
2013-08-25 22:55:19 -0400

    Bump version to 0.1.1.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='mcgill-minerva',
-    version='0.1.0',
+    version='0.1.1',
     description='Client library for McGill Minerva.',
     url='https://github.com/isbadawi/minerva',
     author='Ismail Badawi',
     author_email='isbadawi@gmail.com',
+    keywords='mcgill minerva',
     packages=['minerva'],
     scripts=['bin/fetch-mcgill-transcript'],
     license='MIT',
